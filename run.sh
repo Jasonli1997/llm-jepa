dd6196630f97016f7a607b0ac74f5e378ddf25db
Hai Huang <haih@google.com>
2025-09-28 16:12:41 -0700

    last_token for qwen and r1-distilled in run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ run_regular() {
 
   echo "Success Rate: regular ${base_model_name} lr=${learning_rate} e=${epoch} lt=${last_token} p=${predictors} s=${seed} lbd=${lbd} dataset=${dataset}" >> output.txt
   torchrun --nproc_per_node=8 finetune.py \
-    --train_file ${dataset}_train.jsonl --eval_file ${dataset}_test.jsonl \
+    --train_file ${dataset}_train.jsonl \
     --output_dir=./fine-tuned --num_epochs=${epoch} --finetune_seed=${seed} --regular \
     --model_name=${base_model_name} --learning_rate=${learning_rate}
   python evaluate.py --model_name=./fine-tuned \
@@ -31,7 +31,7 @@ run_jepa() {
 
   echo "Success Rate: jepa ${base_model_name} lr=${learning_rate} e=${epoch} lt=${last_token} p=${predictors} s=${seed} lbd=${lbd} dataset=${dataset}" >> output.txt
   torchrun --nproc_per_node=8 finetune.py \
-    --train_file ${dataset}_train.jsonl --eval_file ${dataset}_test.jsonl \
+    --train_file ${dataset}_train.jsonl \
     --output_dir=./fine-tuned --num_epochs=${epoch} --finetune_seed=${seed} \
     --last_token=${last_token} --lbd=${lbd} --predictors=${predictors} \
     --model_name=${base_model_name} --learning_rate=${learning_rate}
@@ -48,6 +48,12 @@ run_jepa() {
 # then
 #   last_token=-4
 # elif [[ "$base_model_name" == allenai/OLMo-2* ]]
+# then
+#   last_token=-1
+# elif [[ "$base_model_name" == Qwen/Qwen* ]]
+# then
+#   last_token=-3
+# elif [[ "$base_model_name" == deepseek-ai/DeepSeek* ]]
 # then
 #   last_token=-1
 # else
